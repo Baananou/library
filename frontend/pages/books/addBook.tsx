88eec04f67037923fa85f0ace46b67bbbb0bcc67
Baananou <Mohamedyessine.BAANANOU@polytechnicien.tn>
2023-05-19 02:14:29 +0100

    added add book page

---
+++ frontend/pages/books/addBook.tsx
@@ -0,0 +1,13 @@
+import { Key } from "react";
+import Layout from "../../components/layout";
+import Link from "next/link";
+import { Router } from "next/router";
+
+const AddBook = () => {
+	return (
+		<Layout>
+			<h1 className="text-2xl font-bold mx-12 pt-6">Add Book Page </h1>
+		</Layout>
+	);
+};
+export default AddBook;
